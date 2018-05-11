449ad44843e7b2f2a6361ea997c4c9c803ee9bdc
HavenDV <havendv@gmail.com>
2018-05-12 01:56:24 +1000

    Added application

---
+++ CodeMetricsApplication/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using CodeMetricsLibrary;
+
+namespace CodeMetricsApplication
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var metrics = new DzhilbMetrics(TextTextBox.Text);
+
+                OperatorsLabel.Content = metrics.OperatorsCount;
+                ClAbsoluteLabel.Content = metrics.ClAbsolute;
+                ClLabel.Content = metrics.Cl;
+                FModLabel.Content = metrics.FMod;
+                LIfLabel.Content = metrics.LIf;
+                LModLabel.Content = metrics.LMod;
+                LLoopLabel.Content = metrics.LLoop;
+                FOpLabel.Content = metrics.FOp;
+
+                DictionaryTextBox.Text = metrics.GetLinesText();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString(), "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
