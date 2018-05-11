7410376233684f3f3737d5d7828a33f3e9c4495d
HavenDV <havendv@gmail.com>
2018-05-12 01:56:18 +1000

    Added new tests

--- CodeMetricsLibrary.Tests/DzhlibMetricsTests.cs
+++ CodeMetricsLibrary.Tests/DzhlibMetricsTests.cs
@@ -8,9 +8,7 @@ namespace CodeMetricsLibrary.Tests
     public class DzhlibMetricsTests
     {
         [TestMethod]
-        public void FirstTest()
-        {
-            var metrics = new DzhilbMetrics(@"using System;
+        public void FirstTest() => BaseTest(@"using System;
 
 class Operator
 {
@@ -44,30 +42,93 @@ class Operator
         rep = char.Parse(Console.ReadLine());
         if (rep == 'Y' || rep == 'у') goto REPEAT;
     }
-}");
+}", 94);
 
-            //foreach (var line in metrics.Methods.SelectMany(i => i.DescendantNodes()))
+        [TestMethod]
+        public void SecondTest() => BaseTest(@"using System;
+
+class Exempl
+{
+    public static int[] Copy(int[] a)
+    {
+        int[] b = new int[a.Length];
+        int j = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] >= 0)
             {
-                //Console.WriteLine($"{line.GetType()} {DzhilbMetrics.GetLine(line).start}     {line}");
+                b[j] = a[i];
+                j++;
             }
-
-            ShowDictionary(metrics);
-
-            Assert.AreEqual(94, metrics.OperatorsCount);
         }
 
-        private static string ToString((int start, int end)[] lines) => string.Join(", ",
-            lines.Select(i =>
-                i.start == i.end
-                    ? $"{i.start}"
-                    : $"{i.start}({i.end})"));
+        return b;
+    }
 
-        private static void ShowDictionary(DzhilbMetrics metrics)
+    public static void Zapoln(ref int[] a, Random g)
+    {
+        for (int i = 0; i < a.Length; i++)
         {
-            foreach (var (name, lines, count) in metrics.Lines)
+            a[i] = g.Next(-100, 300);
+        }
+    }
+
+    public static void Print(int[] a, string str, string strl)
+    {
+        Console.WriteLine(strl);
+        for (int i = 0; i < a.Length; i++)
+        {
+
+            if (a[i] != 0) Console.Write(str, a[i]);
+
+            Console.WriteLine();
+        }
+    }
+
+    public static void Main()
+    {
+        int[] a, b, c, p;
+        Random g = new Random();
+        char r;
+        do
+        {
+            Console.Clear();
+
+
+
+            Console.WriteLine(""OnpefleaHTe размер первого массива!"");
+            a = new int[int.Parse(Console.ReadLine())];
+            Console.WriteLine(""OnpeflenHTe размер второго массива!"");
+            b = new int[int.Parse(Console.ReadLine())];
+            c = new int[a.Length + b.Length];
+            Exempl.Zapoln(ref a, g);
+            Exempl.Zapoln(ref b, g);
+            Exempl.Print(a, "" {0,5}"", ""Первый исходный массив!!!"");
+            Exempl.Print(b, "" {0,5}"", ""Второй исходный массив!!!"");
+            p = Exempl.Copy(a);
+            Array.Copy(p, 0, c, 0, a.Length);
+            p = Exempl.Copy(b);
+            Array.Copy(p, 0, c, a.Length, b.Length);
+            Exempl.Print(c, "" {0,5}"", ""Результатный массив!!!"");
+            Console.WriteLine();
+            Console.WriteLine(""BbinonHHTb повтор программы? Y/N"");
+            r = char.Parse(Console.ReadLine());
+        } while (r == 'Y' || r == 'у');
+    }
+}", 94);
+        
+        private static void BaseTest(string text, int expected)
+        {
+            var metrics = new DzhilbMetrics(text);
+
+            foreach (var line in metrics.Methods.SelectMany(i => i.DescendantNodes()))
             {
-                Console.WriteLine($"{name} | {ToString(lines)} | {count}");
+                Console.WriteLine($"{line.GetType()} {DzhilbMetrics.GetLine(line).start}     {line}");
             }
+
+            Console.WriteLine(metrics.GetLinesText());
+
+            Assert.AreEqual(expected, metrics.OperatorsCount);
         }
     }
 }
